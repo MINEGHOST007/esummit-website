adf031713b5ef02f65e7e44339684c231169499f
Mohammad Taha <tmohammed771@gmail.com>
2025-02-21 09:31:30 +0530

    scanner working

--- qr/static/qr/js/qr-scan.js
+++ qr/static/qr/js/qr-scan.js
@@ -8,14 +8,21 @@ const scanner = new Html5QrcodeScanner('scan-reader', {
 scanner.render(success, error);
 
 function success(result) {
+
     // document.getElementById('result').innerHTML = `<h1 class="text-lg lg:text-2xl uppercase font-bold text-center">Success : ${result}</h1>`;
     // document.getElementById('result').value = result;
     // scanner.clear();
     // document.getElementById('scan-reader').remove();
     // document.getElementById('qr-form').submit();
-    console.log(result)
-    window.location.href = `scan/${result}`;
+    
+    const baseUrl = 'https://esummit.edcnitd.co.in/qr/scan/';
+    
+    if (result.startsWith(baseUrl)) {
+        const uniqueCode = result.substring(baseUrl.length).split('/')[0];
+        window.location.href = `/scan/${uniqueCode}`;
+    }
 }
 
-function error(error){
+function error(error) {
+    console.error(error);
 }
